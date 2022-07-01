14a9b15b35e467d7a4875b795863806267c65476
geekbrains <igor_sharin@mail.ru>
2022-07-01 07:52:06 +0300

    третье число

---
+++ 013/Program.cs
@@ -0,0 +1,14 @@
+﻿void  ThirdNumber(int x) {
+    int div = 1;
+    while (x / div > 10) {
+        div *= 10;
+    }
+    if (div < 100) {
+        Console.WriteLine("Маленькое число");
+
+    }else {
+        int digit = (x / (div/100))% 10;
+        Console.WriteLine("3 число {0}", digit);
+    }
+}
+ThirdNumber(14657);
