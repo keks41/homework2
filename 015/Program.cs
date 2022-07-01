9dcdea6398c2f1cca1b62eb07d6f48765402cf26
geekbrains <igor_sharin@mail.ru>
2022-07-01 08:10:06 +0300

    Выходной

---
+++ 015/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] day = {"Понедельник", "вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресение"};
+Console.WriteLine("enter number (1-7)") ;
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0 && number < 8)
+{
+    Console.Write("Сегодня "+ day[number-1]);
+    if (number == 6 || number == 7) {
+        Console.Write("Выходной");
+    }
+
+
+}
+
+
+
