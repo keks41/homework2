ac9372c2abe982ed27538dd84f7c2e29ad6f0f4e
geekbrains <igor_sharin@mail.ru>
2022-07-04 19:34:24 +0300

    021

---
+++ 021/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("enter number :");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] A = new int[num];
+for(int i = 1; i <= num; i++) 
+{
+    A[i-1] = i*i*i;
+    Console.WriteLine(A[i-1]);
+}
