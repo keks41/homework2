f8ad244977aee9dcc0c5710d6f08f69c67ca288b
geekbrains <igor_sharin@mail.ru>
2022-07-04 18:59:53 +0300

    023

---
+++ 023/Program.cs
@@ -0,0 +1,11 @@
+﻿double number(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double  result = 0;
+    result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2) );
+
+
+
+    return result;
+}
+ double S = number(3, 6, 8, 2, 1, -7);
+ Console.WriteLine(S);
