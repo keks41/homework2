4ee9a7b2635741f6336a602103000468d6b246c2
geekbrains <igor_sharin@mail.ru>
2022-07-05 20:46:05 +0300

    озведение в степень

---
+++ 025/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число 1 ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2 ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for(int i = 1; i <= B; i++)
+{
+     result = result * A; 
+}
+Console.Write($" Число {A} в степени {B} = {result}"); 
