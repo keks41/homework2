23567a569eec7d41d51026b935dfdeb9bf01a809
geekbrains <igor_sharin@mail.ru>
2022-07-05 19:26:24 +0300

    Ispalindrome

--- 019/Program.cs
+++ 019/Program.cs
@@ -1,8 +1,28 @@
-﻿int array
-if (A[0] == A[4] && A[1] == A[3])
-{
-    Console.Write("yes");
+﻿bool isPalindrome(int x){
+    int div = 1;
+
+    while ((x/div)>=10) div *=10;
+    
+    while(x!=0)
+    {
+        int l = x / div;
+        int r = x % 10;
+        if(l!=r)
+        {
+            Console.WriteLine(" Not Palindrome");
+            return false;
+        }
+        x = (x % div) /10;
+        div/=100;
+        
+    } 
+    Console.WriteLine("number isPalindrome");
+    return true;
+        
+    
 }
-else{
-    Console.Write("no");
-}
+isPalindrome(12321);
+
+
+
+
