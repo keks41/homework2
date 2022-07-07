59218395b6595d822269096700be8602a8572ae1
geekbrains <igor_sharin@mail.ru>
2022-07-07 20:16:02 +0300

    Сумма цифр в числе

---
+++ 027/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+Console.Write("Введите число  :");
+int X = Convert.ToInt32(Console.ReadLine());
+int sum = 0 ;
+
+int result (int X)
+{
+    while ( X > 0)
+    {
+        sum = sum + X % 10;
+        X /= 10;
+    }
+    return sum;
+}
+int res = result(X);
+Console.WriteLine($"Сумма цифр в числе равно {res}");
