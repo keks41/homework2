f6820200a243c9f2d38811b22bab3f0ef0109f75
geekbrains <igor_sharin@mail.ru>
2022-07-09 11:22:32 +0300

    random array

---
+++ 029/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[8];
+Random rnd = new Random();
+
+void PrintArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0-10, 10);
+        Console.Write($"{array[i]} , ");
+    }
+}
+PrintArr(array);
