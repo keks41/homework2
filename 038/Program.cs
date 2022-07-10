e5318d1e06eeef453e632a9a703fe9fca4c5dbf9
geekbrains <igor_sharin@mail.ru>
2022-07-10 15:25:50 +0300

    max-min

---
+++ 038/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+int[] array = {3,7,22,2,78};
+int max = array[0];
+int min = array[0];
+int sum =0;
+for(int i = 0 ; i<array.Length; i++)
+{
+    if(array[i]>max) max=array[i];
+    if(array[i]<min) min = array[i];
+    sum = max-min;
+}
+Console.WriteLine(sum);
