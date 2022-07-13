5b6738ec8d0036d04226ac6a0102fe444c1ea034
geekbrains <igor_sharin@mail.ru>
2022-07-13 21:03:01 +0300

    числа больше 0

---
+++ 041/Program.cs
@@ -0,0 +1,15 @@
+﻿
+    Console.WriteLine("Введите размерность массива :");
+    int len = Convert.ToInt32(Console.ReadLine());
+    int[] arr = new int[len];
+    int count = 0;
+    
+        for (int i = 0; i < arr.Length; i++ )
+        {
+            Console.WriteLine($"Введите значение для элемента [{i}] "); 
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+            if (arr[i] > 0) count++;
+
+        } 
+    
+     Console.WriteLine(count);
