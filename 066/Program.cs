cee73127e23a8e31d0c91c0ea7e91c5f452411a3
geekbrains <igor_sharin@mail.ru>
2022-07-26 21:34:00 +0300

    Рекурсия M + N

--- 066/Program.cs
+++ 066/Program.cs
@@ -1,20 +1,8 @@
 ﻿//Задача 66: Задайте значения M и N. Напишите программу,
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
 
-//M = 1; N = 15 -> 120
-//M = 4; N = 8. -> 30
-/*System.Console.WriteLine("Введите M :");
-int M = int.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите N :");
-int N = int.Parse(Console.ReadLine());
-int PrintSumNumbers(int M, int N)
-{
-    if (N < M) return;
-    return PrintSumNumbers(M + (N - 1));
 
-}
-System.Console.WriteLine(PrintSumNumbers(M, N));
-*/
+
 System.Console.WriteLine("Введите M :");
 int M = int.Parse(Console.ReadLine());
 System.Console.WriteLine("Введите N :");
